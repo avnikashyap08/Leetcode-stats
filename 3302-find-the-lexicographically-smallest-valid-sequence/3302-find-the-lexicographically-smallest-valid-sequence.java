d8eefa764983e385d6738f66b695b134489e43e5
Avni Kashyap <100591268+avnikashyap08@users.noreply.github.com>
2026-08-09 01:23:09 +0530

    Time: 37 ms (44.74%), Space: 137.5 MB (47.37%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.java
@@ -1,11 +1,8 @@
 class Solution {
   public int[] validSequence(String word1, String word2) {
     int[] ans = new int[word2.length()];
-    // last[j] := the index i of the last occurrence in word1, where
-    // word1[i] == word2[j]
     int[] last = new int[word2.length()];
     Arrays.fill(last, -1);
-
     int i = word1.length() - 1;
     int j = word2.length() - 1;
     while (i >= 0 && j >= 0) {
@@ -13,7 +10,6 @@ class Solution {
         last[j--] = i;
       --i;
     }
-
     boolean canSkip = true;
     j = 0;
     for (i = 0; i < word1.length(); ++i) {
@@ -26,7 +22,6 @@ class Solution {
         ans[j++] = i;
       }
     }
-
     return j == word2.length() ? ans : new int[0];
   }
 }
