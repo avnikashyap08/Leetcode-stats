eb61bc094f1959eabdc8ccce82deae4610d4afb3
Avni Kashyap <100591268+avnikashyap08@users.noreply.github.com>
2026-08-17 00:17:27 +0530

    Time: 4 ms (60%), Space: 18.3 MB (20%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.c
@@ -0,0 +1,9 @@
+#include <stdbool.h>
+bool stoneGameIX(int* stones, int stonesSize) {
+    int cnt[3] = {0, 0, 0};
+    for (int i = 0; i < stonesSize; i++)
+        cnt[stones[i] % 3]++;
+    if (cnt[0] % 2 == 0)
+        return cnt[1] > 0 && cnt[2] > 0;
+    return abs(cnt[1] - cnt[2]) > 2;
+}
